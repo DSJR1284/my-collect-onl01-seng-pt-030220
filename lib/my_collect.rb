6c134ae93322299c8d5556e7ec084b5f8d258913
Doug Jones <dsjr1284@gmail.com>
2020-03-13 17:40:44 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,32 +4,9 @@ def my_collect(students)
   while x < students. length 
   collect << yeild (students [x])
   x += 1 
-end 
+  end 
  collect
- end 
+end 
 
  
  
-# def hello(array)
-#   i = 0
-#   collection = []
-#   while i < array.length
-#     collection << yield(array[i])
-#     i += 1
-#   end
-#   collection
-# end
-
-# def my_collect(array)
-#   i = 0
-#   name_collection = []
-#   while i < array.length
-#     name_collection.push yield(array[i])
-#   # you could also do it this way:
-#   # name_collection << yield(array[i])
-#     i += 1
-#   end
-#   name_collection
-# end
-
-# my_collect(list) {|i| i.split(" ").first}
