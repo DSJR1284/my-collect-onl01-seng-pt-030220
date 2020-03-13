af30484b5ab6c191a9ad335230812c8250a2c9fe
Doug Jones <dsjr1284@gmail.com>
2020-03-13 17:46:43 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,11 +2,11 @@ def my_collect(students)
    x = 0 
   collect = []
   while x < students. length 
-  collect << yeild (students [x])
+  collect << yeild (students [s])
   x += 1 
   end 
  collect
 end 
 
- my_collect(students) {|x| .split(" ").first}
+ my_collect(students) {|s| x.split(" ").first}
  
